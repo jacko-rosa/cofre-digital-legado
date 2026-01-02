fd80e7cda55e549761f196eef4bd1a1f09f060bd
cwi.jrosa <jackslfmr@gmail.com>
2026-01-02 00:41:11 -0300

    CDL-1: user find by id

--- src/controller/UserControler.ts
+++ src/controller/UserControler.ts
@@ -15,10 +15,22 @@ class UserController {
             const createdUser = await UserService.register(user);
             Log.endInfo(CLASS, METHOD, createdUser);
             res.status(201).json(createdUser);
-
         } catch (error) {
             Log.error(CLASS, METHOD, 'Error registering user', error);
-            // todo: HandleError(error, res);
+            res.status(500).json({ message: 'Erro interno' });
+        }
+    }
+
+    async getUserById(req: Request<{ id: string }>, res: Response): Promise<UserDto | void> {
+        const METHOD = 'getUserById';
+        try {
+            Log.startInfo(CLASS, METHOD, req.params);
+            const user = await UserService.getUserById(req.params.id);
+            Log.endInfo(CLASS, METHOD, user);
+            res.status(200).json(user);
+        } catch (error) {
+            Log.error(CLASS, METHOD, 'Error getting user by id', error);
+            res.status(500).json({ message: 'Erro interno' });
         }
     }
 }
